2dcf6a0ab2c70bcb1e8575a20afdb020c87d9dbc
user.email <kushbhardwaj8800@gmail.com>
2024-12-06 11:34:44 +0530

    check

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+function App() {
+  return (
+   <>
+    
+      <Router>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
+   </>
+  );
+}
+
+
+
+
+
+export default App;
---
+++ src/component/AdminAuth/AdminAuth.js
@@ -0,0 +1,8 @@
+import {CvPath} from '../../utils/utils'
+export default function Signup(){
+    return (
+        <>
+            <h1>Hi admin</h1>
+        </>
+    )
+}
